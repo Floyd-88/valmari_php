9fec3d44fa565abbd72d8ae7c23fc0e4c539b7df
Dio Eracler <dioeracler1988@gmail.com>
2023-11-23 00:45:11 +0300

    sec_6

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
     // direction: 'vertical', // вертикальная прокрутка
 	slidesPerView: 3, // показывать по 3 превью
 	spaceBetween: 54, // расстояние между слайдами
@@ -18,9 +18,24 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.slider__next',
       prevEl: '.slider__back',
     },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+});
+
+new Swiper('.swiper_2', {
+  // direction: 'vertical', // вертикальная прокрутка
+slidesPerView: 2, // показывать по 3 превью
+spaceBetween: 150, // расстояние между слайдами
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination',
+    clickable: true,
+    width: 15,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider__next',
+    prevEl: '.slider__back',
+  },
 });
