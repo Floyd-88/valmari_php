266e6df0e70667f7b87ef1a1669d629678a10b35
Dio Eracler <dioeracler1988@gmail.com>
2023-11-26 21:00:17 +0300

    page_2 sec_2

--- js/swiper.js
+++ js/swiper.js
@@ -1,61 +1,62 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
-new Swiper('.swiper', {
-    // direction: 'vertical', // вертикальная прокрутка
-	slidesPerView: 3, // показывать по 3 превью
-	spaceBetween: 54, // расстояние между слайдами
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.pagination',
-      clickable: true,
-      width: 15,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider__next',
-      prevEl: '.slider__back',
-    },
-});
-
-new Swiper('.swiper_2', {
+new Swiper(".swiper", {
   // direction: 'vertical', // вертикальная прокрутка
-slidesPerView: 2, // показывать по 3 превью
-spaceBetween: 150, // расстояние между слайдами
+  slidesPerView: 3, // показывать по 3 превью
+  spaceBetween: 54, // расстояние между слайдами
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.pagination',
+    el: ".pagination",
     clickable: true,
     width: 15,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__next',
-    prevEl: '.slider__back',
+    nextEl: ".slider__next",
+    prevEl: ".slider__back",
   },
 });
 
-new Swiper('.swiper_3', {
+new Swiper(".swiper_2", {
   // direction: 'vertical', // вертикальная прокрутка
-slidesPerView: 3, // показывать по 3 превью
-spaceBetween: 30, // расстояние между слайдами
+  slidesPerView: 2, // показывать по 3 превью
+  spaceBetween: 150, // расстояние между слайдами
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.pagination',
+    el: ".pagination",
     clickable: true,
     width: 15,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__next',
-    prevEl: '.slider__back',
+    nextEl: ".slider__next",
+    prevEl: ".slider__back",
   },
 });
+
+new Swiper(".swiper_3", {
+  // direction: 'vertical', // вертикальная прокрутка
+  slidesPerView: 3, // показывать по 3 превью
+  spaceBetween: 30, // расстояние между слайдами
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+    width: 15,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider__next",
+    prevEl: ".slider__back",
+  },
+});
+console.log('good')
