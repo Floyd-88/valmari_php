1d799bda6a097ba00c2d01c3b3ff5369f2aab78b
Dio Eracler <dioeracler1988@gmail.com>
2023-11-28 12:59:02 +0300

    page_3 sec_3

--- js/close_program_course.js
+++ js/close_program_course.js
@@ -1,14 +1,11 @@
-// let close = document.querySelector(".close_program");
-let cont = document.querySelector("#cont");
-
-
 document.addEventListener("click", (event) => {
-    console.log(event.target.className)
+    let elem = event.target.parentElement.nextElementSibling
+
     if(event.target.className === 'close_program') {
         event.target.classList.toggle('rotate_btn')
-        cont.classList.toggle("content_none")
+        elem.classList.toggle("content_none")
     } else if(event.target.className === 'close_program rotate_btn') {
         event.target.classList.toggle('rotate_btn')
-        cont.classList.toggle("content_none")
+        elem.classList.toggle("content_none")
     } 
 })
